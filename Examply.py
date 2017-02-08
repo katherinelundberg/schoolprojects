ff5b58e9c90b08f84dd34c87ca8ad39f6009e2ef
katherinelundberg <baugher.katherine@gmail.com>
2017-02-07 21:21:10 -0800

    Add files via upload

---
+++ Examply.py
@@ -0,0 +1,11 @@
+vocabulary_words = {"banana": 1, "cake": 15, "pillow": 50, "button": 0.5}
+maximum = None
+
+# for value in vocabulary_words.values():
+#     print value
+#     if value > maximum:
+#         maximum = value
+
+# # if value > maximum:
+# print maximum
+print sorted(vocabulary_words.values())[-1]
